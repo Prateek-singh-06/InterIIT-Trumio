0c39ced2a138dd80c5eea533f0833d5e33635f55
Prateek kumar singh <113665258+Prateek-singh-06@users.noreply.github.com>
2023-12-17 03:15:29 +0530

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -36,8 +36,7 @@ app.use(useRouterSaveDetails);
 
 mongoose
   .connect(
-    "mongodb+srv://prateek:ARub4LN122y0sxmC@cluster0.hg316ou.mongodb.net/INTER-IIT-TRUMIO?retryWrites=true&w=majority"
-    // MONGO_URL
+    MONGO_URL
   )
   .then(() => {
     app.listen(PORT, () => {
